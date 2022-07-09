db1b6d61b53fc375df5a6f4f5f817f934f15fccd
Patrick Roelofs <contact@patrickroelofs.com>
2022-07-09 22:34:56 +0200

    check if on netflix detailed page and get title from movie

---
+++ src/services/netflix/Netflix.js
@@ -0,0 +1,30 @@
+// List of functions to run while observing the page
+const observeBody = new MutationObserver(function (mutations, observer) {
+  let isDetailedPage = checkIfDetailedPage();
+
+	if(isDetailedPage) {
+		let detailedPage = parseDetailedPage(isDetailedPage);
+
+		console.log(detailedPage);
+	}
+});
+
+function checkIfDetailedPage() {
+  let matched = window.location.href.match(/jbv=(\d+)/);
+
+  return matched !== null ? matched[1] : false;
+}
+
+function parseDetailedPage(movieId) {
+	let titleEle = document.querySelector("img.previewModal--player-titleTreatment-logo");
+	let title = titleEle.getAttribute("alt").trim();
+	return {
+		title: title,
+	}
+}
+
+// Start observing Netflix body content.
+observeBody.observe(document.body, {
+  childList: true,
+  subtree: true,
+});
