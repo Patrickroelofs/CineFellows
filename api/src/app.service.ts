7eb61dc75473f29619476afcce76e66d6c81a0cf
Patrick Roelofs <contact@patrickroelofs.com>
2022-07-10 15:57:54 +0200

    test api grab imdb score

--- api/src/app.service.ts
+++ api/src/app.service.ts
@@ -1,8 +1,22 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { firstValueFrom, map } from 'rxjs';
 
 @Injectable()
 export class AppService {
-  testIMDBRequest(): string {
-    return 'Hello World!';
+  constructor(private readonly httpService: HttpService) {}
+
+  async testIMDBRequest(): Promise<any> {
+    const response = await firstValueFrom(
+      this.httpService
+        .get('https://www.imdb.com/title/tt0111161/')
+        .pipe(map((res) => res.data)),
+    );
+
+    const match = response.match(/"ratingValue":(\d.\d)/);
+
+    return {
+      imdbScore: match[1],
+    };
   }
 }
