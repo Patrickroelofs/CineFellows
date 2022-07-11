2f7249f5e5f80822fbf85d94dc4b18d129444404
Patrick Roelofs <contact@patrickroelofs.com>
2022-07-11 23:36:28 +0200

    very buggy working version

--- src/services/netflix/Netflix.js
+++ src/services/netflix/Netflix.js
@@ -1,16 +1,17 @@
 let data;
 
 // List of functions to run while observing the page
-const observeBody = new MutationObserver(function (mutations, observer) {
+const observeBody = new MutationObserver(async function (mutations, observer) {
   let isDetailedPage = checkIfDetailedPage();
 
 	if(isDetailedPage) {
-
 		let detailedPage = parseDetailedPage();
 
     if(!data) {
-      data = fetchData(detailedPage);
+      data = await fetchData(detailedPage);
     }
+
+    await displayDataOnDetailedPage(detailedPage);
 	}
 });
 
@@ -29,7 +30,35 @@ function parseDetailedPage() {
 	}
 }
 
+function searchDataByName(key, inputArray) {
+  for(let i = 0; i < inputArray.length; i++) {
+    if(inputArray[i].netflixName === key) {
+      console.log(inputArray[i]);
+      return inputArray[i];
+    }
+  }
+}
+
+async function displayDataOnDetailedPage(detailedPage) {
+  const singleton = searchDataByName(detailedPage.title, data);
+
+  if(document.body.getElementsByClassName('TEST').length >= 1) {
+    console.log("Yeppers")
+  } else {
+    // TODO: I was here
+    console.log("Noppers")
+    let container = document.querySelector(".previewModal--player-titleTreatmentWrapper");
+    container.setAttribute("class", "previewModal--player-titleTreatmentWrapper TEST");
+    container.setAttribute("style", "background-color: #000; color: #ff0000; font-size: 64px;");
+    
+    let newEl = document.createElement("div");
+    newEl.appendChild(document.createTextNode(singleton.imdbRating));
+    container.appendChild(newEl);
+  }
+}
+
 async function fetchData(detailedPage) {
+  console.log("Fetching data...");
   const URL = "http://localhost:3000/netflix/" + detailedPage.title;
 
   const response = await fetch(URL, { mode: "cors" });
